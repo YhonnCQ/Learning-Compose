713bf72d9cfb2ddfa3a42ab03d34112a56d242a5
YhonnCQ <yhonncq@gmail.com>
2021-10-05 22:57:17 -0500

    feat: States by Click

--- app/src/main/java/dev/yhonncq/learningcompose/MainActivity.kt
+++ app/src/main/java/dev/yhonncq/learningcompose/MainActivity.kt
@@ -3,36 +3,99 @@ package dev.yhonncq.learningcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.yhonncq.learningcompose.ui.theme.LearningComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LearningComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MyScreenContent()
             }
         }
     }
 }
 
 @Composable
+fun MyApp(content: @Composable () -> Unit) {
+    LearningComposeTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = List(size = 100) { "Android #$it" }) {
+    val counterState = remember { mutableStateOf(0) }
+    Column(modifier = Modifier.fillMaxSize()) {
+        NameList(names = names, modifier = Modifier.weight(weight = 1f))
+        Counter(
+            count = counterState.value,
+            updateCount = { newCount ->
+                counterState.value = newCount
+            }
+        )
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    var isSelected by remember { mutableStateOf(false) }
+    val backgroundColor by animateColorAsState(
+        targetValue = if (isSelected) MaterialTheme.colors.secondaryVariant else Color.Transparent
+    )
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier
+            .padding(all = 24.dp)
+            .background(color = backgroundColor)
+            .clickable(onClick = {
+                isSelected = !isSelected
+            })
+    )
+}
+
+@Composable
+fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    Button(
+        onClick = { updateCount(count+1) },
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = if (count > 5) MaterialTheme.colors.secondary else MaterialTheme.colors.primary
+        )
+    ) {
+        Text(text = "I've been clicked $count times")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    LearningComposeTheme {
-        Greeting("Android")
+    MyApp {
+        MyScreenContent()
     }
 }
